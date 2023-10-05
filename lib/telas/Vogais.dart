71dfd421acb5e51ef9c808a1e24ffb2655b333af
Gabriel Assys Mendes Oliveira <110845125+GABRIEL-ASSYS@users.noreply.github.com>
2023-10-05 13:10:35 -0300

    Update Vogais.dart

--- lib/telas/Vogais.dart
+++ lib/telas/Vogais.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class Vogais extends StatefulWidget {
   @override
@@ -6,8 +7,54 @@ class Vogais extends StatefulWidget {
 }
 
 class _VogaisState extends State<Vogais> {
+  final player = AudioPlayer();
+
+  //Função para executar o audio
+  void _executar(String nomeAudio) {
+    player.play(AssetSource("audios/${nomeAudio}.mp3"));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    //Obtendo as dimensões da tela
+    double largura = MediaQuery.of(context).size.width;
+    double altura = MediaQuery.of(context).size.height;
+
+    return GridView.count(
+      crossAxisCount: 2,
+      childAspectRatio: MediaQuery.of(context).size.aspectRatio * 2,
+      children: <Widget>[
+        GestureDetector(
+          onTap: () {
+            _executar("a");
+          },
+          child: Image.asset("assets/imagens/a.png"),
+        ),
+        GestureDetector(
+          onTap: () {
+            _executar("e");
+          },
+          child: Image.asset("assets/imagens/e.png"),
+        ),
+        GestureDetector(
+          onTap: () {
+            _executar("i");
+          },
+          child: Image.asset("assets/imagens/i.png"),
+        ),
+        GestureDetector(
+          onTap: () {
+            _executar("o");
+          },
+          child: Image.asset("assets/imagens/o.png"),
+        ),
+        GestureDetector(
+          onTap: () {
+            _executar("u");
+          },
+          child: Image.asset("assets/imagens/u.png"),
+        ),
+      ],
+    );
   }
 }
